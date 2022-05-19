11ae6917df1a98af901667b55d23fd577b492cd6
Eric Ferrari Ramos <102629741+ericferrari93@users.noreply.github.com>
2022-05-19 19:12:46 -0300

    Add files via upload

---
+++ o.o/datas.py
@@ -0,0 +1,14 @@
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        print('Contruindo data {}'.format(self))
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def formatada(self):
+        print('{}/{}/{}'.format(self.dia, self.mes, self.ano))
+
+
+
+
